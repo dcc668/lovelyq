74b75eb8626b70ab192e157053348e96cd84f6dd
dcc668 <1187053696@qq.com>
2016-12-19 14:37:26 +0800

    1

---
+++ src/com/example/Test/Test.java
@@ -0,0 +1,10 @@
+package com.example.Test;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("----------------");
+    }
+}
